99ffe2fa0748d2348eaac5ae8e96fa31b8395efd
mrproblemsolver <102019204+mrproblemsolver@users.noreply.github.com>
2023-01-23 14:33:59 +0300

    Create p0067.cpp

---
+++ C++/p0067.cpp
@@ -0,0 +1,13 @@
+/*
+ https://projecteuler.net/problem=67
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string answer() {
+ return "Sağlamalı djikstra ya da mozaik üçgen diziler yöntemi eniyiler. Brute force olmadan cevap kesinlikle bilinemez.";
+ }
+int main() {
+ cout << answer() << endl;
+ }
