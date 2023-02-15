33e37b0702c42a0f4e3291953eb4c96dffdfe01c
mrproblemsolver <102019204+mrproblemsolver@users.noreply.github.com>
2023-02-15 08:23:29 +0300

    Create p0027.h

---
+++ C/p0027.h
@@ -0,0 +1,58 @@
+/*
+ https://projecteuler.net/problem=27
+ */
+#include <stdio.h>
+int maxa = 999;
+int mina = -999;
+int maxb = 1000;
+int minb = -1000;
+bool isprime(long long int result) {
+ if (result % 2 == 0) return false;
+ for (long long int i = 3; (i * i) < result; i += 2) {
+  if (result % i == 0) return false;
+  }
+ return true;
+ }
+long long int answer() {
+ int b = 0;
+ int n = 0;
+ bool processloop = false;
+ long long int result = 0;
+ long long int ret = 0;
+ int maxn = -1;
+ for (int a = mina; a <= maxa; a++) {
+  for (b = minb; b <= maxb; b++) {
+   acceptableloop = true;
+   processloop = false;
+   for (n = 0; acceptableloop; n++) {
+    result = (n*n)+(n*a)+b;
+    acceptableloop = isprime(result);
+    if (acceptableloop) processloop = true;
+    }
+   n = n - 2;
+   if (n > maxn) {
+    maxn = n;
+    ret = maxn * b;
+    if (ret < 0) ret = ret * -1;
+    }
+   if (processloop) {
+    acceptableloop = true;
+    for (n = 0; acceptableloop; n--) {
+     result = (n*n)+(n*a)+b;
+     acceptableloop = isprime(result);
+     }
+    n = (n + 2) * -1;
+    if (n > maxn) {
+     maxn = n;
+     ret = maxn * b;
+     if (ret < 0) ret = ret * -1;
+     }
+    }
+   }
+  }
+ return ret;
+ }
+int main(void) {
+ printf("%lld", answer());
+ return 0;
+ }
